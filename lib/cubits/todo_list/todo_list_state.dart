7a9e5d2252cd8d969764a16241b119b9a124d2c7
kondiq22 <61063578+kondiq22@users.noreply.github.com>
2022-04-05 01:21:39 +0200

    Add cubit todo_list with void addTodo editTodo toggleTodo removeTodo

---
+++ lib/cubits/todo_list/todo_list_state.dart
@@ -0,0 +1,30 @@
+part of 'todo_list_cubit.dart';
+
+class TodoListState extends Equatable {
+  final List<Todo> todos;
+  TodoListState({
+    required this.todos,
+  });
+
+  factory TodoListState.initial() {
+    return TodoListState(todos: [
+      Todo(id: '1', desc: 'First test task.'),
+      Todo(id: '2', desc: 'New task to do.'),
+      Todo(id: '3', desc: 'Do sth.'),
+    ]);
+  }
+
+  TodoListState copyWith({
+    List<Todo>? todos,
+  }) {
+    return TodoListState(
+      todos: todos ?? this.todos,
+    );
+  }
+
+  @override
+  String toString() => 'TodoListState(todos: $todos)';
+
+  @override
+  List<Object> get props => [todos];
+}
